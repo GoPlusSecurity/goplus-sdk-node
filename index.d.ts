f313f4c69166195457f4d4c8d770df076142e94d
cong <1461561010@qq.com>
2023-09-15 17:43:12 +0800

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,4 @@
-import { GoPlus } from './src/script/api';
-import { ErrorCode } from './src/script/errorcode';
-
 declare module '@goplus/sdk-node' {
-    export const GoPlus: GoPlus;
-    export const ErrorCode: ErrorCode;
+    export const GoPlus: object;
+    export const ErrorCode: object;
 }
