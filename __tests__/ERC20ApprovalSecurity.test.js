cd09375a3010bf638ebadd254e3e9da4a255f9c2
cong <1461561010@qq.com>
2023-09-25 11:08:56 +0800

    fix

--- __tests__/ERC20ApprovalSecurity.test.js
+++ __tests__/ERC20ApprovalSecurity.test.js
@@ -2,5 +2,5 @@ import { GoPlus, ErrorCode } from '../src';
 
 test('ERC20 Approval Security', async () => {
   let data = await GoPlus.erc20ApprovalSecurity('56', '0xd018e2b543a2669410537f96293590138cacedf3', 30);
-  expect(data.code).toBe(ErrorCode.SUCCESS);
+  expect([ErrorCode.SUCCESS, ErrorCode.DATA_PENDING_SYNC]).toContain(data.code);
 });
--- example/ERC20ApprovalSecurity.ts
+++ example/ERC20ApprovalSecurity.ts
@@ -3,7 +3,7 @@ import { GoPlus, ErrorCode } from '@goplus/sdk-node';
 async function test() {
   let res = await GoPlus.erc20ApprovalSecurity('56', '0xd018e2b543a2669410537f96293590138cacedf3', 30);
 
-  if (res.code !== ErrorCode.SUCCESS) {
+  if (res.code !== ErrorCode.SUCCESS && res.code !== ErrorCode.DATA_PENDING_SYNC) {
     console.error(res.message);
   } else {
     console.log(res);
